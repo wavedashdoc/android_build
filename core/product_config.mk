6f2ea4570f9a8f82811918b436ac557234aa5902
Ricardo Cerqueira <github@cerqueira.org>
2017-08-22 23:06:24 +0200

    Preliminary support for product builds
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/product_config.mk
+++ core/product_config.mk
@@ -175,12 +175,23 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+ifneq ($(strip $(TARGET_BUILD_APPS)),)
+# An unbundled app build needs only the core product makefiles.
+all_product_configs := $(call get-product-makefiles,\
+    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+else
+ ifneq ($(PIXELDUST_BUILD),)
+    $(call import-products, device/pixeldust/$(PIXELDUST_BUILD)/device.mk)
+    all_product_configs := device/pixeldust/$(PIXELDUST_BUILD)/device.mk
+  else
+    # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    all_product_configs := $(get-all-product-makefiles)
+  endif # PIXELDUST_BUILD
+endif
 
 all_named_products :=
-
+ifeq ($(PIXELDUST_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -199,6 +210,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
