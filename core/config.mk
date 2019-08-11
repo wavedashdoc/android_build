717219e91e3a20d724b596bd4b3237f898cd8b1d
Kshitij Gupta <kshitijgm@gmail.com>
2019-08-11 18:10:32 +0530

    config: Include custom boardconfig a bit later
    - We're trying to export MAKE_PREBUILT to soong
    - Let's include it right before soong_config.mk is included
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/config.mk
+++ core/config.mk
@@ -230,10 +230,6 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-ifneq ($(PIXELDUST_BUILD),)
-include vendor/pixeldust/config/BoardConfigPD.mk
-endif
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1178,6 +1174,10 @@ dont_bother_goals := out \
     recoveryimage-nodeps \
     vbmetaimage-nodeps \
     product-graph dump-products
+
+ifneq ($(PIXELDUST_BUILD),)
+include vendor/pixeldust/config/BoardConfigPD.mk
+endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
