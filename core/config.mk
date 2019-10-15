1ae7024f830a7a5540828082a679d11762dec237
spezi77 <spezi7713@gmx.net>
2019-10-15 10:41:47 +0200

    build: core: Enable pixeldust sepolicy.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/config.mk
+++ core/config.mk
@@ -1187,7 +1187,7 @@ endif
 ifneq ($(PIXELDUST_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-# $(eval include device/pixeldust/sepolicy/common/sepolicy.mk)
+$(eval include device/pixeldust/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
